da89b2b5c50707232e2c0eb0abf1fdc5b73bb06e
erlanggachan <chandra.nugroho.e@gmail.com>
2017-04-25 23:05:57 +0700

    Make Over
    + Visualisasi Grid
    + Button (that isn't working yet) :"(

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 <head>
 <title>Spending</title>
-<link rel="stylesheet" href="css/materialize.css">
+<link rel="stylesheet" href="css/materialize.min.css">
 </head>
 <body>
+
+<!--Navigation Bar-->
+
  <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Logo Spending</a>
@@ -14,26 +17,104 @@
       </ul>
     </div>
   </nav>
+  
+<!--Body-->
+  
   <div class="container">
 	<div class="row">
-		<h3>Recent Spending</h3>
-	</div>
-	<div class="row">
-		<h5>Tanggal</h5>
-		<ul class="collection">
-			<li class="collection-item">
-				<span class="title">Nominal</span>
-				<p>Nama Pengeluaran <br>
-				 Deskripsi
-				</p>
-			</li>
-			<li class="collection-item">
-				<span class="title">Nominal</span>
-				<p>Nama Pengeluaran<br>
-				 Deskripsi
-				</p>
-			</li>
-	</div>
+		<div class="col s12 m8 l8">
+			<div>
+				<h5>Your Visualization</h5>
+			</div>
+			
+			<div class="col s12">
+			  <div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+				  <span class="card-title">Visualization</span>
+				  <p>Visualisasi data sebulan terakhir.</p>
+				</div>
+			  </div>
+			</div>
+		</div>
+		
+		<div class ="col s12 m4 l4">
+			<div>
+				<h5>Category Visualization</h5>
+			</div>
+			
+			<div class="col s12">
+			  <div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+				  <span class="card-title">Visualization</span>
+				  <p>Visualisasi data tag</p>
+				</div>
+			  </div>
+			</div>
+			
+			<div class="col s12">
+			  <div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+				  <span class="card-title">Visualization</span>
+				  <p>Visualisasi data tag</p>
+				</div>
+			  </div>
+			</div>	
+		</div>
+      </div>
   </div>
+  
+  <div class="fixed-action-btn">
+    <a class="btn-floating btn-large red" href="#trxmodal">
+      <i class="large material-icons">+</i>
+    </a>
+  </div>
+  
+  <!-- Modal Structure -->
+		<div id="trxmodal" class="modal">
+			<div class="modal-content">
+			  <div class="row">
+			    <form class="col s12" method="post">			      
+			      <div class="row">
+			        <div class="input-field col s12">
+			          <select name="category">
+			            <option value="" disabled selected>Choose your option</option>
+						<?php
+							$category=cat_disp('category','asc');
+							print_r($category);
+							foreach ($category as $row) {
+								echo '<option value='.$row['id_category'].'>'.$row['category'].'</option>';
+							}
+						?>
+					  </select>
+			          <label for="last_name">Category</label>
+			        </div>
+			      </div>
+
+			      <div class="row">
+			        <div class="input-field col s12">
+			          <input id="last_name" type="text" class="validate" name="amount">
+			          <label for="last_name">Amount</label>
+			        </div>
+			      </div>
+
+			      <div class="row">
+			        <div class="col s12">
+			          <button id="trx_submit" class="btn waves-effect waves-light right" type="submit" name="submit">Submit</button>
+			        </div>
+			      </div>
+			    </form>
+			  </div>
+		    </div>
+		</div>'
+		
+	<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+    	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    		$('#trxmodal').modal();
+    		$('select').material_select();
+  		});
+	</script>
 </body>
 </html>
